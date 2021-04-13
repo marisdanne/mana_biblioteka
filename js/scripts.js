9eb7e057b2d4409319473bd6888dd134ffe3d2a0
maris.danne <maris.danne@gmail.com>
2021-04-13 16:16:39 +0300

    gramatu pievienosana

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,36 @@
+const POP_UP = document.getElementById('popUp');
+let gramatas = [];
+
 document.getElementById('jaunaGramata').addEventListener('click', () => {
-    console.log("Poga nospiesta!");
+    POP_UP.style.display = 'block';
 
-    let popUp = document.getElementById('popUp');
-
-    popUp.style.display = 'block'
-    
 })
+
+document.getElementById('pievienotGramatu').addEventListener('click', () => {
+    POP_UP.style.display = 'none';
+
+    let gramata = {virsraksts: virsraksts.value, autors: autors.value};
+
+    virsraksts.value = "";
+    autors.value = "";
+
+    gramatas.push(gramata);
+
+    render();
+})
+
+
+function render() {
+    let biblioteka = document.getElementById('biblioteka');
+    biblioteka.innerHTML = "";
+
+    for(let i = 0; i < gramatas.length; i++) {
+        let gramata = `
+        <div class="gramata">
+            <h3>Virsraksts: ${gramatas[i].virsraksts}</h4>
+            <h4>Autors: ${gramatas[i].autors}</h4>
+        </div>`;
+
+        biblioteka.innerHTML += gramata;
+    }
+}
